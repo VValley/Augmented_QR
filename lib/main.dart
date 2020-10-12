a1eb67e3a3cafd87acda3848386e73838621b3bd
Henrik Vedal <vedalhenrik@gmail.com>
2020-10-12 18:54:01 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'material_barcode_scanner.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Widget build(BuildContext context) {
+    return CameraPreviewScanner();
+  }
+}
---
+++ lib/material_barcode_scanner.dart
@@ -0,0 +1,166 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:camera/camera.dart';
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import 'detector_painters.dart';
+import 'scanner_utils.dart';
+
+class CameraPreviewScanner extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _CameraPreviewScannerState();
+}
+
+class _CameraPreviewScannerState extends State<CameraPreviewScanner> {
+  List<Barcode> _scanResults;
+  CameraController _camera;
+  Detector _currentDetector = Detector.text;
+  bool _isDetecting = false;
+  CameraLensDirection _direction = CameraLensDirection.back;
+
+  final BarcodeDetector _barcodeDetector =
+      FirebaseVision.instance.barcodeDetector();
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeCamera();
+  }
+
+  void _initializeCamera() async {
+    final CameraDescription description =
+        await ScannerUtils.getCamera(_direction);
+
+    _camera = CameraController(
+      description,
+      defaultTargetPlatform == TargetPlatform.iOS
+          ? ResolutionPreset.low
+          : ResolutionPreset.max,
+    );
+    await _camera.initialize();
+
+    _camera.startImageStream((CameraImage image) {
+      if (_isDetecting) return;
+
+      _isDetecting = true;
+
+      ScannerUtils.detect(
+        image: image,
+        detectInImage: _barcodeDetector.detectInImage,
+        imageRotation: description.sensorOrientation,
+      ).then(
+        (dynamic results) {
+          if (_currentDetector == null) return;
+          setState(() {
+            _scanResults = results;
+          });
+        },
+      ).whenComplete(() => _isDetecting = false);
+    });
+  }
+
+  Widget _buildResults() {
+    const Text noResultsText = Text('No results!');
+
+    if (_scanResults == null ||
+        _camera == null ||
+        !_camera.value.isInitialized) {
+      return noResultsText;
+    }
+
+    CustomPainter painter;
+
+    final Size imageSize = Size(
+      _camera.value.previewSize.height,
+      _camera.value.previewSize.width,
+    );
+
+    if (_scanResults is! List<Barcode>) return noResultsText;
+    painter = BarcodeDetectorPainter(imageSize, _scanResults);
+
+    return CustomPaint(
+      painter: painter,
+    );
+  }
+
+  Widget _buildImage() {
+    return Container(
+      constraints: const BoxConstraints.expand(),
+      child: _camera == null
+          ? const Center(
+              child: Text(
+                'Initializing Camera...',
+                style: TextStyle(
+                  color: Colors.green,
+                  fontSize: 30.0,
+                ),
+              ),
+            )
+          : Stack(
+              fit: StackFit.expand,
+              children: <Widget>[
+                CameraPreview(_camera),
+                _buildResults(),
+              ],
+            ),
+    );
+  }
+
+  void _toggleCameraDirection() async {
+    if (_direction == CameraLensDirection.back) {
+      _direction = CameraLensDirection.front;
+    } else {
+      _direction = CameraLensDirection.back;
+    }
+
+    await _camera.stopImageStream();
+    await _camera.dispose();
+
+    setState(() {
+      _camera = null;
+    });
+
+    _initializeCamera();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ML Vision Example'),
+      ),
+      body: Stack(
+        children: [
+          _buildImage(),
+          Text(
+            _scanResults.length.toString(),
+            style: Theme.of(context)
+                .textTheme
+                .headline4
+                .apply(color: Colors.white),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _toggleCameraDirection,
+        child: _direction == CameraLensDirection.back
+            ? const Icon(Icons.camera_front)
+            : const Icon(Icons.camera_rear),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _camera.dispose().then((_) {
+      _barcodeDetector.close();
+    });
+
+    _currentDetector = null;
+    super.dispose();
+  }
+}
